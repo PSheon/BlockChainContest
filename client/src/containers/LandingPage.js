20c804fcebaed85e9b4f585c9f4bdfc1ebf9638d
PSheon <pauljiang61020@gmail.com>
2017-12-01 18:27:06 +0800

    fixed

--- client/src/containers/LandingPage.js
+++ client/src/containers/LandingPage.js
@@ -10,6 +10,7 @@ class LandingPage extends Component {
     super(props);
 
     this.state = {
+      peerName: '',
       ein: ''
     }
   }
@@ -27,11 +28,30 @@ class LandingPage extends Component {
     this.props.checkUserEin();
   }
 
-  renderButton = () => {
-    return (
-      <button type="submit" className="btn waves-effect waves-light col s12 yellow darken-4" style={{ borderRadius: '40px' }}>連接節點</button>
-    )
+  renderPeerPanel = () => {
+    if (this.state.peerName !== '') {
+      return (
+        <div className="row margin">
+          <div className="col s12">
+            <div className="card-panel teal accent-4 center" style={{ borderRadius: '40px' }}>
+              <h3>{this.state.peerName}</h3>
+            </div>
+          </div>
+        </div>
+      )
+    }
   }
+
+  renderButton = () => (
+    this.props.peerDetailList.map((peerDetail) => {
+      if (peerDetail.GUInumber === this.state.ein) {
+        this.setState({ peerName: peerDetail.name });
+        <button type="submit" className="btn waves-effect waves-light col s12 yellow darken-4" style={{ borderRadius: '40px' }}>連接節點</button>
+      } else {
+        this.setState({ peerName: '' });
+      }
+    })
+  )
 
   render() {
     return (
@@ -40,6 +60,7 @@ class LandingPage extends Component {
           <div className="col s12 white grey-text text-darken-1" style={{ borderRadius: '40px', padding: '5vh' }}>
             <form action="/" onSubmit={this.handleSubmit}>
               <h3>請輸入您的統一編號與私鑰</h3>
+              {this.renderPeerPanel()}
               <div className="row margin">
                 <Input s={12} label="統一編號" type="number" name="ein" onChange={(e) => this.setState({ ein: e.target.value })} required>
                   <Icon>account_circle</Icon>
@@ -63,9 +84,10 @@ class LandingPage extends Component {
   };
 };
 
-function mapStateToProps({ ein }) {
+function mapStateToProps({ ein, peer }) {
   return {
     isEin: ein.isEin,
+    peerDetailList: peer.peerDetailList
   }
 }
 
