996217ab6fd859d7d9d03684d2ca590aaa2e58f9
PSheon <pauljiang61020@gmail.com>
2017-12-01 21:55:27 +0800

    test

--- client/src/components/Dashboard/TraceBlock.js
+++ client/src/components/Dashboard/TraceBlock.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import QrReader from 'react-qr-reader';
 
+import Block from '../Dashboard/Block/Block';
 
 class TraceBlock extends Component {
   constructor(props) {
@@ -10,9 +13,36 @@ class TraceBlock extends Component {
 
   render() {
     return (
-      <div>TraceBlock</div>
+      <div className="container">
+        <div className="row">
+          <div className="input-field col s12">
+            <a className="btn waves-effect waves-light col s12 teal accent-4 modal-trigger" href="#scanModal" style={{ borderRadius: '40px' }}>ＱＲ掃描</a>
+          </div>
+        </div>
+        <div className="row">
+          {this.props.blocks.map((block, i) => {
+            <Block key={i} block={block} />
+          })}
+        </div>
+        <div id="scanModal" className="modal scanModal">
+          <div className="modal-content">
+            <QrReader
+              delay={this.state.delay}
+              onError={this.handleError}
+              onScan={this.handleScan}
+              style={{ width: '100%' }}
+            />
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
-export default TraceBlock;
+function mapStateToProps({ block }) {
+  return {
+    blocks: block.blocks
+  }
+}
+
+export default connect(mapStateToProps)(TraceBlock);
